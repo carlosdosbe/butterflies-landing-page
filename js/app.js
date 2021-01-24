30c6cc805811071c185af2687ffaba2ac8910e06
Carlos Daniel OB <carlosdosbe@gmail.com>
2021-01-24 16:59:47 -0600

    Final desktop version 👀

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var swiper = new Swiper('.swiper-container', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 'auto',
+  coverflowEffect: {
+    rotate: 0,
+    stretch: 0,
+    depth: 200,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  loop: true,
+});
